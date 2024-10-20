94f1894d91685048cdfb85454da8f5a4761f9a3f
Donald <donumeh54@gmail.com>
2024-10-20 22:54:43 +0100

    More involved typpe annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -12,6 +12,14 @@ T = TypeVar("T")
 def safely_get_value(
     dct: Mapping, key: Any, default: Union[T, None] = None
 ) -> Union[Any, T]:
+    """
+    safely_get_value: the function to annotate
+
+    Args:
+        dct type(dict)
+        key type(any)
+        default type(T | None)
+    """
     if key in dct:
         return dct[key]
     else:
