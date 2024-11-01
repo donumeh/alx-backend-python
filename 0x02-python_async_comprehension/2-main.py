3a602d918bb9b7dabc4a3c563b03e7fce170fa55
Donald <donumeh54@gmail.com>
2024-11-01 12:43:14 +0100

    run time for four parallel comprehension

---
+++ 0x02-python_async_comprehension/2-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import asyncio
+
+measure_runtime = __import__('2-measure_runtime').measure_runtime
+
+
+async def main():
+    return await(measure_runtime())
+
+print(asyncio.run(main()))
